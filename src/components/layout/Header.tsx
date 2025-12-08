76d571a1aa3862b0db08145981134407944f4b3b
jun <znznun@gmail.com>
2025-12-09 01:59:09 +0900

    fix(Header): Header border 제거

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ export function Header() {
 
   return (
     <>
-      <header className="bg-sky-200 shadow-sm border-b border-gray-200 sticky top-0 z-50">
+      <header className="bg-sky-200 shadow-sm sticky top-0 z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <h1 className="text-2xl font-bold text-gray-900 flex items-center">
