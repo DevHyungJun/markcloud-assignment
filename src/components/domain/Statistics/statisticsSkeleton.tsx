3a4a4f0a6e23339540c7eecd89ed73b7f3e835e9
jun <znznun@gmail.com>
2025-12-09 08:53:58 +0900

    feat(statistics): 스켈레톤 애니메이션 추가

--- src/components/domain/Statistics/statisticsSkeleton.tsx
+++ src/components/domain/Statistics/statisticsSkeleton.tsx
@@ -6,7 +6,6 @@ const StatisticsSkeleton = () => {
           key={index}
           className="relative w-full h-[400px] bg-gray-200 rounded-lg overflow-hidden"
         >
-          {/* Shimmer 애니메이션 */}
           <div
             className="absolute inset-0 bg-linear-to-r from-transparent via-white/60 to-transparent animate-[shimmer_2s_infinite]"
             style={{
