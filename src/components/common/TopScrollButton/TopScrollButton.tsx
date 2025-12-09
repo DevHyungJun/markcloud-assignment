0fe813f1a2a2cb6d73c728a8fc4845e7dbea7528
jun <znznun@gmail.com>
2025-12-09 09:19:46 +0900

    style(TopScrollButton): 버튼 투명도 추가

--- src/components/common/TopScrollButton/TopScrollButton.tsx
+++ src/components/common/TopScrollButton/TopScrollButton.tsx
@@ -34,7 +34,7 @@ const TopScrollButton = () => {
     <button
       onClick={handleClick}
       className={cn(
-        "fixed bottom-5 right-5 bg-sky-400 text-white p-1 rounded-full shadow-lg transition-all duration-300 w-12 h-12 flex items-center justify-center z-40",
+        "fixed bottom-5 right-5 bg-sky-400/65 text-white p-1 rounded-full shadow-lg transition-all duration-300 w-12 h-12 flex items-center justify-center z-40",
         "hover:bg-sky-700"
       )}
       aria-label="맨 위로 스크롤"
