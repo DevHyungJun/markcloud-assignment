17b017ef550a74bec2f601333982a019925e50db
jun <znznun@gmail.com>
2025-12-09 09:14:29 +0900

    style(Header): 배경 투명도 설정

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -34,7 +34,7 @@ export function Header() {
   };
 
   return (
-    <header className="bg-sky-200 shadow-sm sticky top-0 z-50">
+    <header className="bg-sky-300/75 shadow-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <Link
